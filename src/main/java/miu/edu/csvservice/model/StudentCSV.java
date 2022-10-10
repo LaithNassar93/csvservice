69d4e107f24284ce505dff9638e9dc95fad24407
LaithNassar93 <laith.nassar93@gmail.com>
2022-10-09 21:44:51 -0500

    Second Commit

---
+++ src/main/java/miu/edu/csvservice/model/StudentCSV.java
@@ -0,0 +1,12 @@
+package miu.edu.csvservice.model;
+
+import lombok.Data;
+
+@Data
+public class StudentCSV {
+
+    private String firstname;;
+    private String lastname;
+    private double gpa;
+    private int age;
+}
